dacfccea7b536d53b9e245ebe19f06fa63630f2e
El nino <el-nino202020@protonmail.com>
2023-08-20 12:59:39 +0800

    init

---
+++ src/main/java/org/elnino/helper/contest/coding/CodingContestHelperApplication.java
@@ -0,0 +1,13 @@
+package org.elnino.helper.contest.coding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodingContestHelperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodingContestHelperApplication.class, args);
+    }
+
+}
